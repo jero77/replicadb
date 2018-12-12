a43f9ec8a81b911c372202d6978831e36757f3a1
jero77 <jeromario.schaefer@stud.uni-goettingen.de>
2018-12-12 10:05:15 +0100

    safety commit

--- src/main/java/Optimizer.java
+++ src/main/java/Optimizer.java
@@ -1,6 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,50 +28,108 @@ public class Optimizer {
         Connection conn = DriverManager.getConnection("jdbc:ignite:thin://127.0.0.1:10800");
 
 
-        // Metadata & initial fragments
-        DBMetadata meta = new DBMetadata(conn.getMetaData());
-        meta.addTableFragment("TA", "AGE", 0, 20);
-        meta.addTableFragment("TA", "AGE", 21, 40);
-        meta.addTableFragment("TA", "AGE", 41, 60);
-        meta.addTableFragment("TA", "AGE", 61, 80);
-        meta.addTableFragment("TA", "AGE", 81, 100);
+//        // Metadata & initial fragments
+//        DBMetadata meta = new DBMetadata(conn.getMetaData());
+//        meta.addTableFragment("TA", "AGE", 0, 20);
+//        meta.addTableFragment("TA", "AGE", 21, 40);
+//        meta.addTableFragment("TA", "AGE", 41, 60);
+//        meta.addTableFragment("TA", "AGE", 61, 80);
+//        meta.addTableFragment("TA", "AGE", 81, 100);
+//
+//
+//        // Query ...
+//        String query = "SELECT * FROM TA, TB WHERE TA.AGE <= 35 AND TA.ID = TB.IDOFTA";
+//
+//        // ... parsing ...
+//
+//        List<TableFragment> list = meta.findMatchingFragment("TA", "AGE", "<=",35);
+//        System.out.println("Fragments for selection condition TA.AGE <= 35");
+//        if (list == null)
+//            throw new IllegalArgumentException("No matching fragments found for table TA on selection TA.AGE <= 35");
+//        else if (list.size() == 1) {
+//            // only 1 fragment found --> it contains all the tuples we want
+//            System.out.println(list.get(0).getTableName() + ".F" + list.get(0).getFragmentNumber() + " age "
+//                    + list.get(0).getMinvalue() + "-" + list.get(0).getMaxvalue());
+//            // ... query server hosting this fragment ...
+//
+//        } else if (list.size() > 1) {
+//            // multiple fragments found
+//            for (TableFragment frag : list)
+//                System.out.println(frag.getTableName() + ".F" + frag.getFragmentNumber() + " age "
+//                        + frag.getMinvalue() + "-" + frag.getMaxvalue());
+//
+//            // ... query all servers hosting those fragments ...
+//            // ... collect tuples on one server ...
+//        }
+//
+//
+//        // Query ...
+//        query = "SELECT * FROM TA, TB WHERE TA.AGE >= 21 AND TA.AGE < 45";
+//
+//        // ... parsing ...
+//        list = meta.findMatchingFragment("TA", "AGE", 21, 45);
+//        System.out.println("Fragments for selection condition TA.AGE >= 21 && TA.AGE <= 45");
+//        for (TableFragment frag : list)
+//            System.out.println(frag.getTableName() + ".F" + frag.getFragmentNumber() + " age "
+//                    + frag.getMinvalue() + "-" + frag.getMaxvalue());
+//
+
+
+        // Store some meta-info
+        PreparedStatement prep = conn.prepareStatement("INSERT INTO FRAGMETA (ID, TABLE, ATTRIBUTE, MINVALUE, " +
+                "MAXVALUE) VALUES (?, ?, ?, ?, ?)");
+
+        for (int i = 0; i < 5; i++) {
+            // 5 fragments for TA on TA.AGE: 1-20, 21-40, etc.
+            prep.setInt(1, i);
+            prep.setString(2, "TA");
+            prep.setString(3, "AGE");
+            prep.setInt(4, 20 * i + 1);
+            prep.setInt(5, 20 * (i + 1));
+            prep.executeUpdate();
+        }
+
+
+        // Some co-partitioned meta data
+        prep = conn.prepareStatement("INSERT INTO COMETA (ID, TABLE, JOINATTR, COTABLE, COJOIN) VALUES (?,?,?,?,?)");
+        prep.setInt(1, 0);
+        prep.setString(2, "TB");
+        prep.setString(3, "IDOFTA");
+        prep.setString(4, "TA");
+        prep.setString(5, "ID");
+        prep.executeUpdate();
+
 
 
         // Query ...
         String query = "SELECT * FROM TA, TB WHERE TA.AGE <= 35 AND TA.ID = TB.IDOFTA";
 
         // ... parsing ...
+        // --> attribute AGE, where a fragmentation exists, is a selection condition of the query
+        Statement stmt = conn.createStatement();
+        String queryFrag = "SELECT ID,MINVALUE,MAXVALUE FROM FRAGMETA WHERE TABLE='TA' AND ATTRIBUTE='AGE'";
+        ResultSet res = stmt.executeQuery(queryFrag);
+        while (res.next()) {
+            System.out.println("Fragment (AGE) found: ID=" + res.getInt(1) + ", MIN="
+                    + res.getInt(2) + ", MAX=" + res.getInt(3));
+        }
 
-        List<TableFragment> list = meta.findMatchingFragment("TA", "AGE", "<=",35);
-        System.out.println("Fragments for selection condition TA.AGE <= 35");
-        if (list == null)
-            throw new IllegalArgumentException("No matching fragments found for table TA on selection TA.AGE <= 35");
-        else if (list.size() == 1) {
-            // only 1 fragment found --> it contains all the tuples we want
-            System.out.println(list.get(0).getTableName() + ".F" + list.get(0).getFragmentNumber() + " age "
-                    + list.get(0).getMinvalue() + "-" + list.get(0).getMaxvalue());
-            // ... query server hosting this fragment ...
-
-        } else if (list.size() > 1) {
-            // multiple fragments found
-            for (TableFragment frag : list)
-                System.out.println(frag.getTableName() + ".F" + frag.getFragmentNumber() + " age "
-                        + frag.getMinvalue() + "-" + frag.getMaxvalue());
-
-            // ... query all servers hosting those fragments ...
-            // ... collect tuples on one server ...
+        String queryCo = "SELECT ID, JOINATTR, COJOIN FROM COMETA WHERE TABLE='TB' AND COTABLE='TA'";
+        res = stmt.executeQuery(queryCo);
+        while (res.next()) {
+            System.out.println("Copartition for TB to TA found: ID=" + res.getInt(1) + ", JOIN="
+                    + res.getString(2) + ", COJOIN=" + res.getString(3));
         }
 
 
-        // Query ...
-        query = "SELECT * FROM TA, TB WHERE TA.AGE >= 21 AND TA.AGE < 45";
+        // Query/Join can be processed!
+        // stmt.executeQuery(query);
 
-        // ... parsing ...
-        list = meta.findMatchingFragment("TA", "AGE", 21, 45);
-        System.out.println("Fragments for selection condition TA.AGE >= 21 && TA.AGE <= 45");
-        for (TableFragment frag : list)
-            System.out.println(frag.getTableName() + ".F" + frag.getFragmentNumber() + " age "
-                    + frag.getMinvalue() + "-" + frag.getMaxvalue());
+        // In case of no copartitioning of TB to TA
+        
+
     }
+
+
 
 }
--- src/test/java/IgniteTestDB.java
+++ src/test/java/IgniteTestDB.java
@@ -12,8 +12,18 @@ public class IgniteTestDB {
         // Return connection to the cluster (Port 10800 default for JDBC client)
         Connection conn = DriverManager.getConnection("jdbc:ignite:thin://127.0.0.1:10800");
 
+        // Create tables
         Statement stmt = conn.createStatement();
+        stmt.executeUpdate("DROP TABLE IF EXISTS TA; DROP TABLE IF EXISTS TB");
         stmt.executeUpdate("CREATE TABLE TA ( ID INT PRIMARY KEY, NAME VARCHAR, AGE INT)");
         stmt.executeUpdate("CREATE TABLE TB ( ID INT PRIMARY KEY, IDOFTA INT)");
+
+        // Metadata table
+        stmt.executeUpdate("DROP TABLE IF EXISTS FRAGMETA; DROP TABLE IF EXISTS COMETA;");
+        stmt.executeUpdate("CREATE TABLE FRAGMETA (ID INT PRIMARY KEY, TABLE VARCHAR, ATTRIBUTE VARCHAR, " +
+                "MINVALUE INT, MAXVALUE INT) WITH \"template=replicated,backups=0\"");
+
+        stmt.executeUpdate("CREATE TABLE COMETA (ID INT PRIMARY KEY, TABLE VARCHAR, JOINATTR VARCHAR, " +
+                "COTABLE VARCHAR, COJOIN VARCHAR) WITH \"template=replicated,backups=0\"");
     }
 }
